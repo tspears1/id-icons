575681fee2d88636ce5ff06aa27c3f0a279c01c9
Tim Spears <tspears1@gmail.com>
2024-12-16 16:43:38 -0500

    change icon build

--- icons/icons.ts
+++ icons/icons.ts
@@ -1,10 +1,17 @@
-import { symbolfy } from "../symbolfy.ts"
+import { symbolfy } from '../symbolfy.ts'
 
 const icons = {
     social: {
         facebook: {
-            regular: symbolfy('icon-facebook-regular', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm8,191.63V152h24a8,8,0,0,0,0-16H136V112a16,16,0,0,1,16-16h16a8,8,0,0,0,0-16H152a32,32,0,0,0-32,32v24H96a8,8,0,0,0,0,16h24v63.63a88,88,0,1,1,16,0Z"/></svg>'),
-            bold: symbolfy('icon-facebook-bold', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor"><path d="M128,20A108,108,0,1,0,236,128,108.12,108.12,0,0,0,128,20Zm12,191.13V156h20a12,12,0,0,0,0-24H140V112a12,12,0,0,1,12-12h16a12,12,0,0,0,0-24H152a36,36,0,0,0-36,36v20H96a12,12,0,0,0,0,24h20v55.13a84,84,0,1,1,24,0Z"/></svg>')
+            regular: {
+                id: 'icon-facebook-regular',
+                viewBox: '0 0 256 256',
+                paths: ["<path d='M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm8,191.63V152h24a8,8,0,0,0,0-16H136V112a16,16,0,0,1,16-16h16a8,8,0,0,0,0-16H152a32,32,0,0,0-32,32v24H96a8,8,0,0,0,0,16h24v63.63a88,88,0,1,1,16,0Z'/>"]
+            },
+            bold: {
+                id: 'icon-facebook-bold',
+                paths: ["<path d='M128,20A108,108,0,1,0,236,128,108.12,108.12,0,0,0,128,20Zm12,191.13V156h20a12,12,0,0,0,0-24H140V112a12,12,0,0,1,12-12h16a12,12,0,0,0,0-24H152a36,36,0,0,0-36,36v20H96a12,12,0,0,0,0,24h20v55.13a84,84,0,1,1,24,0Z'/>"]
+            }
         }
     }
 }
